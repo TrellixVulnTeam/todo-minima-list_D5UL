058823748ed94280057201339ac128b9952c43f9
itallosavieira <itallo.sa.vieira@gmail.com>
2021-02-07 17:49:51 -0300

    add logic js

--- assets/components/index.js
+++ assets/components/index.js
@@ -1,10 +1,19 @@
+const formButton = document.querySelector('[data-form-button]');
+
 const addTask = (event) => {
-    event.preventDefault()
-    const inputForm = document.querySelector('[data-form-input')
-    const inputValue = inputForm.value
-    console.log(inputValue)
+    event.preventDefault();
+
+    const formInput = document.querySelector('[data-form-input]');
+    const inputValue = formInput.value;
+    
+    const list = document.querySelector('[data-list]');
+    const task = document.createElement('li');
+    const content = `<p class='list_content'> ${inputValue}</p>`
+    
+    task.className = 'list_item';
+    task.innerHTML = content
+
+    list.appendChild(task);
 }
 
-const btnForm = document.querySelector('[data-form-button]')
-
-btnForm.addEventListener('click', addTask)
+formButton.addEventListener('click', addTask);
