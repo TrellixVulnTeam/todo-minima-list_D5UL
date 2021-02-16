9016fe3d9cf2225f6f3d43e234a4c73dec0e9a40
Itallo Sá <itallosa@pop-os.localdomain>
2021-02-16 12:13:35 -0300

    add view

--- assets/js/controllers/TaskController.js
+++ assets/js/controllers/TaskController.js
@@ -3,28 +3,37 @@ class TaskController {
     constructor() {
 
         const $ = document.querySelector.bind(document);
+
         this._inputForm = $('[data-form-input]');
+        this._buttonDone = $('[data-button-done]');
+        this._buttonDelete = $('[data-button-delete]');
+
         this._taskList = new TaskList();
+        this._taskView = new TaskView($('[data-tasks]'));
+        this._taskView.update(this._taskList);
     }
 
     _addTask(event) {
 
         event.preventDefault();
-        const newTask = new Task(this._inputForm.value);
-
-        this._taskList._addTaskToList(newTask);
-        console.log(this._taskList.list);
-
+        this._addNewTask();
+        this._updateView();
         this._cleanForm();
     }
 
     _addNewTask() {
 
+        const newTask = new Task(this._inputForm.value);
+        return this._taskList._addTaskToList(newTask);
+    }
 
+    _updateView() {
+
+        return this._taskView.update(this._taskList);
     }
 
     _cleanForm() {
-        
+
         this._inputForm.value = '';
         this._inputForm.focus();
     }
--- assets/js/views/TaskView.js
+++ assets/js/views/TaskView.js
@@ -1,11 +1,29 @@
 class TaskView {
 
-    constructor() {
+	constructor(element) {
 
-    }
+		this._element = element;
+	}
 
-    _template() {
+	_template(model) {
 
-        
-    }
+		return `
+			<ul class="tasks_list" data-list>
+				${model.list.map( el => `
+					<li class="task">
+						<p class="task_text">${el.task}</p>
+						<div class="task_buttons">
+							<button class="task_button--done task_button data-button-done"> done </button>
+							<button class="task_button--delete task_button data-button-delete"> delete </button>
+						</div>
+					</li>
+				`).join('')}
+			</ul>
+    `
+	}
+
+	update(model) {
+
+		this._element.innerHTML = this._template(model);
+	}
 }
